50ec4614965e071798a5c4f6048743d0d0bd9b9e
Sivert Utne <sivertutne@Macbook.local>
2020-09-09 22:57:50 +0200

    Customizing even more
    I cant really preview the changes i am doing so this will have to do

--- api/spotify.py
+++ api/spotify.py
@@ -89,14 +89,14 @@ def makeSVG(data):
 
     if data == {} or data["item"] == "None":
         contentBar = "" #Shows/Hides the EQ bar if no song is currently playing
-        currentStatus = "Last seen listening to 🎧"
+        currentStatus = "🎧 Previously vibed to "
         recentPlays = recentlyPlayed()
         recentPlaysLength = len(recentPlays["items"])
         itemIndex = random.randint(0, recentPlaysLength - 1)
         item = recentPlays["items"][itemIndex]["track"]
     else:
         item = data["item"]
-        currentStatus = "Vibing to 🎧"
+        currentStatus = "🎧 Vibing to"
     image = loadImageB64(item["album"]["images"][1]["url"])
     artistName = item["artists"][0]["name"].replace("&", "&amp;")
     songName = item["name"].replace("&", "&amp;")
